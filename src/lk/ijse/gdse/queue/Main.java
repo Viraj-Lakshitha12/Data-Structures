8668ffb8f9cc44ff3b45673680b826be867eb2b0
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-22 13:31:06 +0530

    add validation part

--- src/lk/ijse/gdse/queue/Main.java
+++ src/lk/ijse/gdse/queue/Main.java
@@ -3,11 +3,16 @@ package lk.ijse.gdse.queue;
 public class Main {
     public static void main(String[] args) {
         Queue queue = new Queue(5);
+
+//        queue.deQueue();
+
+
         queue.enQueue(10);
         queue.enQueue(20);
         queue.enQueue(30);
         queue.enQueue(40);
         queue.enQueue(50);
+        queue.enQueue(60);
 
         System.out.println("deQueue : "+queue.deQueue());
         System.out.println("deQueue : "+queue.deQueue());
--- src/lk/ijse/gdse/queue/Queue.java
+++ src/lk/ijse/gdse/queue/Queue.java
@@ -11,13 +11,29 @@ public class Queue {
        rearPoint=-1;
     }
     public void enQueue(int data){
+
+        if (isFull()){
+            grow();
+        }
         if (frontPoint==-1){
             frontPoint=0;
         }
         elementData[++rearPoint]=data;
     }
 
+    private void grow() {
+        int [] temp = elementData;
+        elementData = new int[rearPoint*2];
+        for (int i = 0; i < temp.length; i++) {
+            elementData[i]=temp[i];
+        }
+    }
+
     public int deQueue() {
+        if (isEmpty()){
+            System.out.println("Queue is empty");
+            return -1;
+        }
         return elementData[frontPoint++];
     }
 
@@ -31,6 +47,13 @@ public class Queue {
             System.out.print(elementData[i]+" ");
         }
         System.out.println("\b]");
+    }
+    public boolean isEmpty(){
+        return frontPoint==-1;
+    }
+
+    public boolean isFull(){
+        return elementData.length-1==rearPoint;
     }
 
     public int size() {
