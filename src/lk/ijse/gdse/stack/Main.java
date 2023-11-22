d86f7a3a032a7d8c354f013af9313a6911a47bc0
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-22 11:17:52 +0530

    stack

---
+++ src/lk/ijse/gdse/stack/Main.java
@@ -0,0 +1,44 @@
+package lk.ijse.gdse.stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack stack = new Stack(5);
+
+//        stack.pop();
+
+
+
+//        add element
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        int peek =stack.peek();
+        System.out.println("peek : "+peek);
+
+        stack.push(40);
+        System.out.println("peek : "+stack.peek());
+
+        stack.push(50);
+        System.out.println("peek : "+stack.peek());
+
+//        remove element
+        System.out.println("pop : "+stack.pop()); // 50
+        System.out.println("pop : "+stack.pop()); // 40
+
+
+        System.out.println("peek : "+stack.peek()); // 30
+
+        stack.push(400);
+        stack.push(500);
+        stack.push(600);
+
+//        print elements
+        stack.printStack();
+
+        System.out.println("Size : "+stack.size());
+
+        stack.push(800);
+        System.out.println("Size : "+stack.size());
+    }
+}
---
+++ src/lk/ijse/gdse/stack/Stack.java
@@ -0,0 +1,64 @@
+package lk.ijse.gdse.stack;
+
+import java.util.Arrays;
+
+public class Stack {
+    int [] elementData;
+    int top;
+    private boolean empty;
+
+    public Stack(int initialCount) {
+        elementData=new int[initialCount];
+        top=-1;
+    }
+
+    public void push(int data) {
+        if (isFull()){
+//            System.out.println("Stack is full ");
+            grow();
+        }
+        elementData[++top]=data;
+    }
+
+    private void grow() {
+        int [] temp = elementData;
+
+        elementData=new int[elementData.length*2];
+        for (int i = 0; i < temp.length; i++) {
+            elementData[i]=temp[i];
+        }
+    }
+
+    public int peek() {
+        return elementData[top];
+    }
+
+    public int pop() {
+        if (isEmpty()){
+            throw new RuntimeException("Stack is empty");
+        }
+        return elementData[top--];
+    }
+
+    public void printStack() {
+        System.out.print("[");
+        for (int i = 0; i <= top; i++) {
+            System.out.print(elementData[i]+" ");
+        }
+        System.out.println("\b]");
+
+    }
+
+    public boolean isFull(){
+        return top == elementData.length - 1;
+    }
+
+    public boolean isEmpty() {
+        return top==-1;
+    }
+
+
+    public int  size() {
+        return top+1;
+    }
+}
