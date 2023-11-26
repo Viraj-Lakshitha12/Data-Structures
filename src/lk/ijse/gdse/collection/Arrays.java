6a902a078fbf681e66902464e4d0627bbdc524a1
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-11-26 20:28:25 +0530

    create arrays

---
+++ src/lk/ijse/gdse/collection/Arrays.java
@@ -0,0 +1,12 @@
+package lk.ijse.gdse.collection;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Object[] ob = new Object[5];
+        System.out.println(ob.length);
+        ob[0]=10;
+        ob[1]=10.10;
+        ob[2]="Hello";
+        System.out.println(java.util.Arrays.toString(ob));
+    }
+}
