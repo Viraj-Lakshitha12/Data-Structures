3719a726514a020205a78a6f68ce6a20b1cebbd6
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-02 20:39:32 +0530

    update hashMap

--- src/lk/ijse/gdse/collection/HashMap_Test.java
+++ src/lk/ijse/gdse/collection/HashMap_Test.java
@@ -1,4 +1,28 @@
 package lk.ijse.gdse.collection;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class HashMap_Test {
+    public static void main(String[] args) {
+        Map<Integer,String> hashMap = new HashMap<Integer,String>();
+        hashMap.put(1,"kamal");
+        hashMap.put(2,"Amal");
+        hashMap.put(3,"Amal");
+
+        System.out.println(hashMap); // {1=kamal, 2=Amal, 3=Amal}
+
+        hashMap.put(3,"X");
+        System.out.println(hashMap.entrySet()); //[1=kamal, 2=Amal, 3=X]
+
+        System.out.println("2 value is : "+hashMap.get(2)); // 2 value is : Amal
+
+        hashMap.remove(3);
+        System.out.println(hashMap); // {1=kamal, 2=Amal}
+
+        System.out.println(hashMap.containsKey(2)); // true
+        System.out.println(hashMap.containsKey(5)); // false
+
+    }
+
 }
