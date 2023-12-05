b753d55e4e73ad53031ea0b1478edb7aa088f270
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-05 22:43:23 +0530

    updated

--- src/lk/ijse/gdse/collection/Task.java
+++ src/lk/ijse/gdse/collection/Task.java
@@ -1,14 +1,36 @@
 package lk.ijse.gdse.collection;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Task {
     public static void main(String[] args) {
-        int[] ar = {10, 20, 20, 20, 30, 40};
+        int[] ar = {100, 120, 200, 20, 20, 40};
         HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        List<Integer> newList = new LinkedList<>();
+
         for (int i = 0; i < ar.length; i++) {
             hashMap.put(ar[i],i);
         }
         System.out.println(hashMap.keySet().stream().sorted().toList());
+
+        System.out.println("----------------------------------------------------");
+
+
+
+
+        int[] ar1 = {100, 120, 200, 20, 20, 40};
+        HashMap<Integer, Integer> hashMap1 = new HashMap<Integer, Integer>();
+        List<Integer> newList1 = new LinkedList<Integer>();
+
+        for (int i = 0; i < ar1.length; i++) {
+            if (!hashMap1.containsKey(ar1[i])){
+                hashMap1.put(ar1[i],i);
+                newList1.add(ar1[i]);
+            }
+        }
+        System.out.println("Hash Map :"+hashMap.keySet().stream().sorted().toList());
+        System.out.println("new array list : "+newList1.stream().sorted().toList());
     }
 }
