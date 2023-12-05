3a6145c220bdd3a06e5a3bf4f533d16c445d0e4e
viraj-Lakhitha~ <viraj.lakshitha.22222@gmail.com>
2023-12-05 22:34:56 +0530

    updated

--- src/lk/ijse/gdse/collection/Task.java
+++ src/lk/ijse/gdse/collection/Task.java
@@ -1,4 +1,14 @@
 package lk.ijse.gdse.collection;
 
+import java.util.HashMap;
+
 public class Task {
+    public static void main(String[] args) {
+        int[] ar = {10, 20, 20, 20, 30, 40};
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < ar.length; i++) {
+            hashMap.put(ar[i],i);
+        }
+        System.out.println(hashMap.keySet().stream().sorted().toList());
+    }
 }
